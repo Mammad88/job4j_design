649ec11e06185c5aa09613d7c09636ad9045e769
Mammad88 <bruki.mammad@mail.ru>
2020-08-06 07:46:27 +0300

    "5.1.2. Создать итератор четные числа [#329835]"

---
+++ chapter_001/src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -0,0 +1,58 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Итератор по массиву, возвращаюший только четные числа.
+ *
+ * @author Bruki Mammad.
+ * @version $1.0$
+ * @since 03.08.2020
+ */
+public class EvenNumbersIterator implements Iterator<Integer> {
+    /**
+     * Позиция указателя в массиве.
+     */
+    private int position;
+    /**
+     * Массив с числами.
+     */
+    private final int[] numbers;
+
+    /**
+     * конструктор для инициализаций массива.
+     *
+     * @param numbers - массив.
+     */
+
+    public EvenNumbersIterator(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.changeNextIndexEven() == 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (this.hasNext()) {
+            return this.numbers[position++];
+        } else {
+            throw new NoSuchElementException("Больше нет четных элементов");
+        }
+    }
+
+    private Integer changeNextIndexEven() {
+        int value = -1;
+        for (int index = this.position; index < this.numbers.length; index++) {
+            if (this.numbers[index] % 2 == 0) {
+                position = index;
+                value++;
+                break;
+            }
+        }
+        return value;
+    }
+}
