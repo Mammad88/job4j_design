b2266abd661eb57933319d66ff59caa919196dcb
Mammad88 <bruki.mammad@mail.ru>
2021-02-09 00:06:00 +0300

    "2. Добавить метод boolean isBinary() [#329846] - рефакторинг"

--- chapter_001/src/main/java/ru/job4j/tree/SimpleTree.java
+++ chapter_001/src/main/java/ru/job4j/tree/SimpleTree.java
@@ -11,20 +11,9 @@ import java.util.Optional;
  * @version $1.0$
  * @since 24.01.2021
  */
-public interface SimpleTree<E> {
+public interface SimpleTree<E extends Comparable<E>> {
 
     boolean add(E parent, E child);
 
     Optional<Node<E>> findBy(E value);
-
-    boolean isBinary();
-
-    class Node<E> {
-        final E value;
-        final List<Node<E>> children = new ArrayList<>();
-
-        public Node(E value) {
-            this.value = value;
-        }
-    }
 }
--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -1,18 +1,28 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 /**
  * class Tree - элементарная структура дерево.
  *
  * @author Bruki Mammad (bruki.mammad@mail.ru)
- * @version $1.0$
- * @since 24.01.2021
+ * @version $2.0$
+ * @since 08.02.2021
  */
-class Tree<E> implements SimpleTree<E> {
+class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+
+    /**
+     * Ссылка на корневой элемент дерева.
+     */
     private final Node<E> root;
 
-    Tree(final E root) {
+    /**
+     * Формирует начальное состояние дерева, устанавливает переданный элемент в кач-ве корневого.
+     *
+     * @param root - значение корневого элемента.
+     */
+    Tree(E root) {
         this.root = new Node<>(root);
     }
 
@@ -32,46 +42,52 @@ class Tree<E> implements SimpleTree<E> {
         if (findBy(child).isEmpty()) {
             Optional<Node<E>> parentEl = findBy(parent);
             if (parentEl.isPresent()) {
-                rsl = parentEl.get().children.add(new Node<>(child));
+                rsl = parentEl.get().leaves().add(new Node<>(child));
             }
-        }
-        return rsl;
-    }
-
-    @Override
-    public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.children);
         }
         return rsl;
     }
 
     /**
-     * Проверяем, что дерево бинарное.
+     * Осуществляет поиск заданного элемента в дереве.
      *
-     * @return Бинарное ли дерево.
+     * @param value искомое значение.
+     * @return контейнер, кот. содержит искомый элемент (если присутствует).
      */
     @Override
+    public Optional<Node<E>> findBy(E value) {
+        return this.findNode(eNode -> eNode.eqValue(value));
+    }
+
+    /**
+     * Проверяет является ли дерево бинарным (кол-во потомков каждого узла не превышает два).
+     * С помощью {@link #findNode(Predicate)} ищет узел с кол-вом потомков больше двух.
+     * По наличию/отсутствию такого узла возращает true/false.
+     *
+     * @return true, если узлов с потомками > 2 не найдено.
+     */
     public boolean isBinary() {
-        boolean binary = true;
+        return findNode(eNode -> eNode.leaves().size() > 2).isEmpty();
+    }
+
+    /**
+     * Возвращает узел дерева, удовлетворяющий переданному условию.
+     *
+     * @param predicate условие.
+     * @return контейнер, кот. содержит искомый элемент (если присутствует)
+     */
+    private Optional<Node<E>> findNode(Predicate<Node<E>> predicate) {
+        Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.children.size() > 2) {
-                binary = false;
+            if (predicate.test(el)) {
+                rsl = Optional.of(el);
+                break;
             }
-            data.addAll(el.children);
+            data.addAll(el.leaves());
         }
-        return binary;
+        return rsl;
     }
 }
-
